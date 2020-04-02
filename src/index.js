747e1482fe0097303e27c31d21ff8b9e8266bc6b
solitas <solitas.roh@kakao.com>
2020-04-02 21:34:41 +0900

    footer and auth UI

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './Components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./Components/App";
 import Client from "./Apollo/Client";
 import { ApolloProvider } from "react-apollo-hooks";
+
 ReactDOM.render(
   <ApolloProvider client={Client}>
     <App />
